591f32a0f9723fa6a24c34e89fe2543b16523db5
Benjamin Jacob <bjb@upsource.de>
2018-09-28 11:01:28 +0200

    start soffice in socket accept mode

--- entrypoint-socket.sh
+++ entrypoint-socket.sh
@@ -1,4 +1,4 @@
-/usr/bin/libreoffice --nofirststartwizard --nologo --norestore --invisible --nolockcheck --nodefault --headless --accept="socket,port=8100;urp;StarOffice.ServiceManager"
+/usr/bin/libreoffice --nofirststartwizard --nologo --norestore --invisible --nolockcheck --nodefault --headless --accept="socket,host=0.0.0.0,port=8100;urp;StarOffice.ServiceManager"
 if [ -d "/tmp/hsperfdata_root" ]; then
         rm -rf /tmp/hsperfdata_root
 fi
